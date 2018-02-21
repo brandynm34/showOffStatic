2fccb75c968c7c1b959cbde432177efa16acd085
fox65 <fox65@email.franklin.edu>
2018-02-21 15:01:01 -0800

    use angular themes

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {OverlayContainer} from "~@angular/material/theming";
+import { OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  // use this to set correct theme class on app holder
+  // eg: <div [class]="themeClass">...</div>
+
   title = 'app';
+  themeClass: string;
+  overlayContainer;
+
+  constructor(
+    //private overlayContainer: OverlayContainer
+  ) {}
+
+  ngOnInit(): void {
+    // subscribe to some source of theme change events, then...
+    //this.themeClass = newThemeClass;
+    //this.overlayContainer.themeClass = newThemeClass;
+  }
+  
 }
