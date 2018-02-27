fc1f813a72479c231487a68bc6e8c80242b81157
Vincent Chho <vincec362@gmail.com>
2018-02-27 11:16:44 -0800

    Created portfolio.js in /routes and /models. Hopefully I created my schema correctly, working on the routes at the moment

---
+++ ShowOffAPI/api/routes/portfolio.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const portfolioModel = require('../models/portfolio');
+
+mongoose.connect("mongodb://admin:W18cohort@ds041678.mlab.com:41678/showoff", (err) =>{
+    if (err) {
+        console.log('There was an error connecting to the database. Bummer.')
+    } else {
+        console.log('Connected to the database. Huzzah.');
+    }
+});
+
+const Portfolio = mongoose.model('PORTFOLIO', portfolioModel.portfolioSchema);
+
+router.get('/', function(req, res, next){
+    console.log('Base protfolio activated');
+    
+})
