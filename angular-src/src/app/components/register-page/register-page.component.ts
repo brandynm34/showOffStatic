525973c1d11dee7ff497533a8934e750acc450b3
brandynm34 <brandynm34@yahoo.com>
2018-03-01 17:06:26 -0800

    this probably wont work

--- angular-src/src/app/components/register-page/register-page.component.ts
+++ angular-src/src/app/components/register-page/register-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+// import { PasswordValidationComponent } from '../password-validation/password-validation.component';
+
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  // constructor(formBuilder: FormBuilder) { 
+  //   this.form = formBuilder.group({
+  //     password: [''],
+  //     confirmPassword: ['']
+  //   })
+  // }
+  constructor( fb: FormBuilder){
+    this.form = fb.group({
+      password: ['',[Validators.required ]],
+      confirmPassword: ['', Validators.required]
+    })
+    
+    
 
-  ngOnInit() {
   }
+
+  ngOnInit(){}
+
+  con(){
+    if (this.form.get('password').value === this.form.get('confirmPassword').value){
+      console.log('same')
+      return true 
+    }
+    else{
+      console.log('not same')
+      return false 
+    }
+    // const hasExclamation = input.value.indexOf('!') >= 0;
+
+    // return hasExclamation ? null : { needsExclamation: true }
+  }
+
+  passw(){
+    if (this.form.get('password').value === this.form.get('confirmPassword').value){
+      console.log('same')
+    }
+    else{
+      console.log('not same')
+    }
+  }
+
+
+  
 
 }
