505b9dd583272103181400bd8318fc0b0c3d0ac3
brandynm34 <brandynm34@yahoo.com>
2018-03-02 16:29:45 -0800

    actual public view changes

--- angular-src/src/app/components/public-view/public-view.component.ts
+++ angular-src/src/app/components/public-view/public-view.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 })
 export class PublicViewComponent implements OnInit, AfterViewInit {
 
-  private _numOfSkills = 0;
-  private _numOfProjects = 0;
+  private _numOfSkills = 15;
+  private _numOfProjects = 6;
   public skillsArr = [];
   public projectsArr = [];
   constructor() { }
