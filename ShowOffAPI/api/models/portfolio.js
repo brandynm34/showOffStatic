2375faaf28f2f2203054d470da1577e32f38c877
Christa Fox <christa.r.fox@me.com>
2018-03-05 09:51:49 -0800

    Update portfolio.js
    Question about storage/ retrieval  of the array for this

--- ShowOffAPI/api/models/portfolio.js
+++ ShowOffAPI/api/models/portfolio.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const portfolioSchema = new Schema({
     Email: { type:String, required: true, unique: true},
+//     I think we need the id_ to order the arrays
     AboutBlurb: { type:String, required: false},
     Facebook: { type:String, required: false},
     Twitter: { type:String, required: false},
