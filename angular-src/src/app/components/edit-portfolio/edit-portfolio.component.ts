1a03e14560d723c11c2ed2129ed5c43bbf4dd6d4
J.R. Ruggiero <fleury14@comcast.net>
2018-03-07 14:53:42 -0800

    api call to get portfolio info successful. fixed png broken links

--- angular-src/src/app/components/edit-portfolio/edit-portfolio.component.ts
+++ angular-src/src/app/components/edit-portfolio/edit-portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // import { DashboardComponent } from './../dashboard/dashboard.component';
+import { JRPortfolioService } from './../../services/portfolio/jr-portfolio-service';
 
 @Component({
   selector: 'app-edit-portfolio',
@@ -10,8 +11,16 @@ export class EditPortfolioComponent implements OnInit {
   public display = 'block';
 
   // constructor(private dash: DashboardComponent) { }
+  constructor ( private _portService: JRPortfolioService) {}
 
   ngOnInit() {
+    // when page loads, grab the necessary values from the database via the service and subscribe to them
+    // NOTE: this will automatically grab the user on the service side, whether that is better or doing it here I'm not sure.
+    this._portService.getPortfolioInfo().subscribe(result => {
+      console.log(result.json());
+      const data = result.json().data;
+    });
+
   }
   closeModal() {
     // this.dash.displayPortfolio = 'none';
