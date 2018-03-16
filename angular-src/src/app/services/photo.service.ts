bfcc523f3fa1dc08124f76077dbf8a59c9bab0e0
J.R. Ruggiero <fleury14@comcast.net>
2018-03-16 12:44:17 -0700

    completed service prototype, probbaly doesnt work

--- angular-src/src/app/services/photo.service.ts
+++ angular-src/src/app/services/photo.service.ts
@@ -2,23 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { environment } from './../../environments/environment';
+import { JRLoginService } from './../services/jr-login-service';
 
 @Injectable()
 
 export class PhotoService {
 
     private _URL = environment.apiURL;
+    private _authState = this._login.getAuth();
 
-    constructor( private _http: HttpClient) {}
+    constructor( private _http: HttpClient, private _login: JRLoginService) {}
 
     public uploadPhoto(file: File) {
         const body = {
-            file: file
+            User_ID : this._authState.id,
+            Photo: 'Photo'
         };
         const HttpOptions = {
             headers: new HttpHeaders({
                 'Content-Type': 'application/json'
-            })
+            }),
+            file: file
         };
         return this._http.post(this._URL + 'api-new/registration/search', body, HttpOptions);
     }
